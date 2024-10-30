0b71757ba9a19de7eed77adeea5e66d564909a0e
nka2001 <89935053+nka2001@users.noreply.github.com>
2024-10-30 18:41:39 -0400

    Server Side - Initial Commit

---
+++ src/main/java/csci610/datastore/DataStore.java
@@ -0,0 +1,17 @@
+package csci610.datastore;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+/**
+ * server class for the database access
+ * @author nicka
+ */
+public class DataStore {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
